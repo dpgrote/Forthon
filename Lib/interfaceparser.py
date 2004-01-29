6744a9ab5e186a8d9b36081224ab2ca625a7dfa3
dave <dave>
2004-01-29 17:25:49 +0000

    Bug fix - attributes for derived type elements was being set incorrectly.

--- Lib/interfaceparser.py
+++ Lib/interfaceparser.py
@@ -1,7 +1,7 @@
 # Created by David P. Grote, March 6, 1998
 # Modified by T. B. Yang, May 19, 1998
 # Parse the interface description file
-# $Id: interfaceparser.py,v 1.1 2004/01/06 18:35:13 dave Exp $
+# $Id: interfaceparser.py,v 1.2 2004/01/29 17:25:49 dave Exp $
 
 # This reads in the entire variable description file and extracts all of
 # the variable and subroutine information needed to create an interface
@@ -133,7 +133,7 @@ def processfile(packname,filename,othermacros=[]):
       tname = string.split(text[:i])[1]
       group = tname
       # Include group name as an attribute
-      attributes = ' '+string.join(g[1:])+' '
+      attributes = ' '+string.join(tname[1:])+' '
       # Create new instance of Ftype and append to the list
       ftype = fvars.Ftype(tname,attributes)
       typelist.append(ftype)
