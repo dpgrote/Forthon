c4838fd57cec108d2815a4c75ae9896f54283995
dave <dave>
2004-02-12 00:17:50 +0000

    Added extra test code to test creation and deletion of derived type in fortran.

--- example2/example2.py
+++ example2/example2.py
@@ -6,7 +6,15 @@ print example2.t1.j,example2.t1.b
 print 'Should be'
 print '5 6.0'
 print ''
+print example2.t2.j,example2.t2.b
+print 'Should be'
+print '25 36.0'
+print ''
 
+testsub3()
+print example2.t2
+print 'Should be'
+print 'unallocated'
 
 
 print 'Tests complete'
