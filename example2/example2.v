1e0e84534a1b0b4c6fc260bd76e98cb8c4ab1628
dave <dave>
2004-02-12 00:16:07 +0000

    Updated the changes that are needed to reflect the changes in Forthon. Now, the New routine does not need to be added to the module. However, the two routines NewPyRefType2 and DelPyRefType2 must be explicitly called in the Fortran.

--- example2/example2.v
+++ example2/example2.v
@@ -6,8 +6,10 @@ b real # Real element of a derived type
 
 ***** Module2:
 t1 Type2 # Test derived type
-t2 _Type2 # Test derived type pointer
+t2 _Type2 # Test derived type pointer. This is included to show the required
+          # way to create and delete a derived type reference.
 
 ***** Subroutines:
 testsub2(ii:integer,aa:real) subroutine
+testsub3() subroutine
 
