be1d4d2378c5099a0c8882c1c76a46d17c6ff87b
dave <dave>
2004-07-29 21:56:54 +0000

    A very simple example

---
+++ simpleexample/example.py
@@ -0,0 +1,6 @@
+from Forthon import *
+from examplepy import *
+
+print "Before setsqrt, x = ",example.x
+setsqrt(10.)
+print "After setsqrt, x = ",example.x
---
+++ simpleexample/example.v
@@ -0,0 +1,9 @@
+example
+
+****** SimpleModule:
+i integer /3/ # Sample integer variable
+x real # Sample real variable
+
+****** SimpleRoutines:
+setsqrt(y:real) subroutine # Sets x to the sqrt(y)
+
