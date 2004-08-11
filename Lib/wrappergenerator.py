5359749b77bd589b7b39103611d32775674ae9e3
dave <dave>
2004-08-11 00:10:58 +0000

    For subroutines which takes derived types in as input, updated the comparison to tp_name to include the package name as the suffix, matching the change made elsewhere.

--- Lib/wrappergenerator.py
+++ Lib/wrappergenerator.py
@@ -2,7 +2,7 @@
 # Python wrapper generation
 # Created by David P. Grote, March 6, 1998
 # Modified by T. B. Yang, May 21, 1998
-# $Id: wrappergenerator.py,v 1.16 2004/08/06 23:31:23 dave Exp $
+# $Id: wrappergenerator.py,v 1.17 2004/08/11 00:10:58 dave Exp $
 
 import sys
 import os.path
@@ -417,8 +417,8 @@ Usage:
           self.cw('  {')
           self.cw('  PyObject *t;')
           self.cw('  t = PyObject_Type(pyobj['+repr(i)+']);')
-          self.cw('  if (strcmp(((PyTypeObject *)t)->tp_name,"Forthon") != 0)'+
-                     'goto err;')
+          self.cw('  if (strcmp(((PyTypeObject *)t)->tp_name,'+
+                        '"Forthon_'+self.pname+'") != 0)'+'goto err;')
           self.cw('  Py_DECREF(t);')
           typename = '((ForthonObject *)pyobj['+repr(i)+'])->typename'
           self.cw('  if (strcmp('+typename+',"'+f.args[i].type+'") != 0) '+
