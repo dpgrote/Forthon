89f85b56cc50f44b7fdf0770d4ea66a8d921d3bc
dave <dave>
2004-10-07 19:58:02 +0000

    Added -F option to getopt list.

--- Lib/preprocess.py
+++ Lib/preprocess.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Created by David P. Grote, March 6, 1998
-# $Id: preprocess.py,v 1.2 2004/01/29 20:40:52 dave Exp $
+# $Id: preprocess.py,v 1.3 2004/10/07 19:58:02 dave Exp $
 
 from cfinterface import *
 import sys
@@ -13,7 +13,7 @@ def py_ifelse(m,v,t,f=''):
     return f
 
 def main():
-  optlist,args=getopt.getopt(sys.argv[1:],"at:",
+  optlist,args=getopt.getopt(sys.argv[1:],"at:F:",
                              ["f90","f90f","2underscores","nowritemodules"])
 
   file = open(args[0],'r')
