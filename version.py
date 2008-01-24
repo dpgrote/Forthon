839906a95b4158d417024f3a2555a95cddb8fb68
dave <dave>
2008-01-24 20:59:22 +0000

    Now also updates the version number in the index.html file.

--- version.py
+++ version.py
@@ -4,15 +4,28 @@ import string
 import version
 
 def update():
-  vv = string.split(version.__doc__,'.')
+  vvold = version.__doc__
+  vv = string.split(vvold,'.')
   vv[2] = str(int(vv[2])+1)
+  vvnew = string.join(vv,'.')
+
+  # --- Update the version number in this file.
   ff = open('version.py','r')
   lines = ff.readlines()
   ff.close()
   ff = open('version.py','w')
-  ff.write('"'+string.join(vv,'.')+'"\n')
+  ff.write('"'+vvnew+'"\n')
   for line in lines[1:]:
     ff.write(line)
+
+  # --- Update the version number in the html file.
+  ff = open('docs/index.html','r')
+  text = ff.read()
+  ff.close()
+  text = text.replace(vvold,vvnew)
+  ff = open('docs/index.html','w')
+  ff.write(text)
+  ff.close()
 
 if __name__ == "__main__":
   print version.__doc__
