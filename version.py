1ba2dfdf28a2714f9467b492cbac43055c1c7074
dave <dave>
2008-01-29 00:57:28 +0000

    Reorganization of the file, now uses indent of 4 spaces.

--- version.py
+++ version.py
@@ -3,29 +3,25 @@
 import string
 import version
 
+def updatefile(filename,vvold,vvnew):
+    # --- Update the version number in the given file.
+    ff = open(filename,'r')
+    text = ff.read()
+    ff.close()
+    text = text.replace(vvold,vvnew)
+    ff = open(filename,'w')
+    ff.write(text)
+    ff.close()
+
 def update():
-  vvold = version.__doc__
-  vv = string.split(vvold,'.')
-  vv[2] = str(int(vv[2])+1)
-  vvnew = string.join(vv,'.')
+    vvold = version.__doc__
+    vv = string.split(vvold,'.')
+    vv[2] = str(int(vv[2])+1)
+    vvnew = string.join(vv,'.')
 
-  # --- Update the version number in this file.
-  ff = open('version.py','r')
-  lines = ff.readlines()
-  ff.close()
-  ff = open('version.py','w')
-  ff.write('"'+vvnew+'"\n')
-  for line in lines[1:]:
-    ff.write(line)
-
-  # --- Update the version number in the html file.
-  ff = open('docs/index.html','r')
-  text = ff.read()
-  ff.close()
-  text = text.replace(vvold,vvnew)
-  ff = open('docs/index.html','w')
-  ff.write(text)
-  ff.close()
+    # --- Update the version number in the files.
+    updatefile('version.py',vvold,vvnew)
+    updatefile('docs/index.html',vvold,vvnew)
 
 if __name__ == "__main__":
-  print version.__doc__
+    print version.__doc__
